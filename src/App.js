6a3fea19e1e6c3b234e4f3d5f53465eda2b90ba6
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2025-05-13 04:52:28 -0300

    no need for starting track

--- src/App.js
+++ src/App.js
@@ -9,14 +9,7 @@ export default function App() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    // 🎵 RADIO SOCKET SETUP
     const radioSocket = new WebSocket("ws://localhost:8000/ws/radio/");
-    const startingTrack = {
-      name: 'Suspenso Pelo Ciclo Implicito',
-      url: "https://storage.googleapis.com/gpgc-api-bucket/RELEASED/GUERRINHA/Suspenso%20Pelo%20Ciclo%20Impli%CC%81cito/Guerrinha%20-%20Suspenso%20Pelo%20Ciclo%20Impli%CC%81cito%20-%2001%20Suspenso%20Pelo%20Ciclo%20Impli%CC%81cito.mp3"
-    };
-
-    dispatch(enqueue(startingTrack));
 
     radioSocket.onmessage = (event) => {
       const data = JSON.parse(event.data);
